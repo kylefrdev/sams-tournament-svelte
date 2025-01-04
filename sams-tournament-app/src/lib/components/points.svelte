00f2f5bdff18fe4aa018f01f9f2bb00ea5f24334
kylefrue <kylefr.junk@gmail.com>
2025-01-04 10:56:41 -0800

    potluck

--- sams-tournament-app/src/lib/components/points.svelte
+++ sams-tournament-app/src/lib/components/points.svelte
@@ -21,6 +21,7 @@
     <table class="table-auto border-collapse border border-gray-500 w-full bg-gray-700" slot="content">
         <thead class="bg-purple-950">
             <tr>
+                <th class="border border-purple-900 text-gray-200 px-4 py-2 text-center">W/L/T</th>
                 <th class="border border-purple-900 text-gray-200 px-4 py-2 text-center">Win</th>
                 <th class="border border-purple-900 text-gray-200 px-4 py-2 text-center">Loss</th>
                 <th class="border border-purple-900 text-gray-200 px-4 py-2 text-center">Tie</th>
@@ -30,6 +31,8 @@
         <tbody class="bg-purple-900">
             {#each points as point}
                 <tr>
+
+                    <td class="border border-purple-800 text-gray-200 px-4 py-2 text-center">{`${point[0]}/${point[1]}/${point[2]}`}</td>
                     <td class="border border-purple-800 text-gray-200 px-4 py-2 text-center">{point[0]}</td>
                     <td class="border border-purple-800 text-gray-200 px-4 py-2 text-center">{point[1]}</td>
                     <td class="border border-purple-800 text-gray-200 px-4 py-2 text-center">{point[2]}</td>
