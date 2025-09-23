af961babdd4364dd041e945706eef988ce6ec585
kylefrue <kylefr.junk@gmail.com>
2025-09-22 22:44:48 -0700

    f

--- sams-tournament-app/src/lib/components/tab.svelte
+++ sams-tournament-app/src/lib/components/tab.svelte
@@ -19,7 +19,6 @@
         padding-right:5px;
         padding-left: 5px;
         flex: 1;         /* Make each button grow equally */
-        width: 100%;     /* Optional: ensures full space inside the flex item */
         /* padding: 10px;   Style as needed */
         text-align: center;
         box-sizing: border-box;
